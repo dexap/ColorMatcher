8aa80f2632b6121fbdc08f2a307262ed303e636e
Benjamin Lassmann <benjamin.lassmann@student.htw-berlin.de>
2022-07-13 14:40:51 +0200

    Initial Commit

---
+++ ColorMatcher/ColorMatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMatcherApp.swift
+//  ColorMatcher
+//
+//  Created by Benjamin Lassmann on 13.07.22.
+//
+
+import SwiftUI
+
+@main
+struct ColorMatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
